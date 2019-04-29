7de2d43f524177286e1bcf19df204d40ec9f63d7
Shai Coleman <shai.coleman@cashanalytics.com>
2019-04-29 15:39:26 +0100

    Custom selenium options

--- spec/system/home_system_spec.rb
+++ spec/system/home_system_spec.rb
@@ -2,7 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'HomeController', type: :system do
   before do
-    driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+    driven_by :selenium, using: :chrome, screen_size: [1400, 1400],
+      options: {
+        desired_capabilities: {
+          chromeOptions: {
+            args: %w[headless disable-gpu]
+          },
+        },
+      }
   end
 
   describe '#index' do

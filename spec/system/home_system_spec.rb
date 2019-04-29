63a6748988057d6263821f16a0fd9b4266fc7eb7
Shai Coleman <shai.coleman@cashanalytics.com>
2019-04-29 14:59:40 +0100

    Replacing with webdrivers gem

--- spec/system/home_system_spec.rb
+++ spec/system/home_system_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'HomeController', type: :system do
   before do
-    driven_by :rack_test
+    driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
   end
 
   describe '#index' do
